0d68fc1a2b844cf92a4c144a798eec8ae4d55dcd
rarchk <tau.ronak@gmail.com>
2017-04-05 22:17:06 +0530

    Final provision

--- provision.sh
+++ provision.sh
@@ -4,6 +4,6 @@ sudo lxd init
 sudo lxc launch ubuntu:16.04 c1 -p default -p docker
 sudo lxc config set c1 security.privileged true
 sudo lxc restart c1
-sudo lxc exec c1 bash 
-export $PARITY_VERSION
-bash 
+sudo lxc exec c1 -- git https://github.com/rarchk/parityCI
+sudo lxc exec c1 -- export $PARITY_VERSION
+sudo lxc exec c1 -- cd parityCI && ./docker_setup.sh 
